cd72a4cc8a23f3a0dbdb91917d6add5721ca0f93
dyhmzall <dyhmzall@gmail.com>
2021-02-19 21:50:43 +0300

    Задание 2

--- Popravko_Mikhail_dz_1/task_1_2.py
+++ Popravko_Mikhail_dz_1/task_1_2.py
@@ -10,7 +10,10 @@
 """
 
 divider = 7
+additional_number = 17
+
 sum_divider = 0
+sum_additional_divider = 0
 
 odd_list = []
 
@@ -19,10 +22,9 @@ for el in range(1, 1000, 2):
     original_number = el ** 3
     odd_list.append(original_number)
 
-    sum_digit = 0
-
     # это число будет делить каждый раз на 10, чтобы получить сумму цифр
     number = original_number
+    sum_digit = 0
 
     while number:
         sum_digit += number % 10
@@ -31,5 +33,17 @@ for el in range(1, 1000, 2):
     if not sum_digit % divider:
         sum_divider += original_number
 
+    # применим тот же алгоритм, функцию не использую, так как мы их еще не проходили
+    number = original_number + additional_number
+    sum_digit = 0
+
+    while number:
+        sum_digit += number % 10
+        number //= 10
+
+    if not sum_digit % divider:
+        sum_additional_divider += original_number
+
 print(f"Список кубов нечетных числе от 0 до 100: {odd_list}")
 print(f"Сумма тех чисел, сумма цифр которых делится на {divider} составляет {sum_divider}")
+print(f"Сумма тех чисел, сумма цифр которых делится на {divider} составляет {sum_additional_divider}")
