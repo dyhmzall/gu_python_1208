66ad84fbcb8f0f243778370824f19dc1fdf2e738
dyhmzall <dyhmzall@gmail.com>
2021-03-08 23:39:40 +0300

    Задания 1 - 5

---
+++ Popravko_Mikhail_dz_5/task_5_1.py
@@ -0,0 +1,31 @@
+"""
+1. Написать генератор нечётных чисел от 1 до n (включительно), используя ключевое слово yield, например:
+odd_to_15 = odd_nums(15)
+next(odd_to_15)
+1
+next(odd_to_15)
+3
+...
+next(odd_to_15)
+15
+next(odd_to_15)
+...StopIteration...
+"""
+
+
+def odd_nums(nums):
+    for num in range(1, nums + 1, 2):
+        yield num
+
+
+odd_to_15 = odd_nums(15)
+
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
---
+++ Popravko_Mikhail_dz_5/task_5_2.py
@@ -0,0 +1,20 @@
+"""
+2. * (вместо 1) Решить задачу генерации нечётных чисел от 1 до n (включительно), не используя ключевое слово yield.
+"""
+
+
+def odd_nums(nums):
+    return (num for num in range(1, nums + 1, 2))
+
+
+odd_to_15 = odd_nums(15)
+
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
